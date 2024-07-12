5f20addb3e8e1a4f546cdd8f05815398acd4b51c
Sezgi İrem İlgar <ilgarsezgiirem@gmail.com>
2024-07-12 09:50:20 +0300

    Initial Commit

---
+++ AnimeApp/AnimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeAppApp.swift
+//  AnimeApp
+//
+//  Created by Sezgi İrem İlgar on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
