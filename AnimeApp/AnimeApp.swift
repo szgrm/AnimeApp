4772405d6b600a64974d38afb6d6cab3054f5207
Sezgi İrem İlgar <ilgarsezgiirem@gmail.com>
2024-08-15 12:02:44 +0300

    add netfox package

--- AnimeApp/AnimeApp.swift
+++ AnimeApp/AnimeApp.swift
@@ -5,10 +5,17 @@
 //  Created by Sezgi İrem İlgar on 12.07.2024.
 //
 
+import netfox
 import SwiftUI
 
 @main
 struct AnimeApp: App {
+    init() {
+        #if DEBUG
+            NFX.sharedInstance().start()
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             TabbarView()
